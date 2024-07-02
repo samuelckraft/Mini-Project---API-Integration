a047ad407851e7a0cfce6682963a033743924df4
samuelckraft <samuelckraft@gmail.com>
2024-07-02 18:57:46 -0400

    update

---
+++ script.js
@@ -0,0 +1,23 @@
+async function fetchPokemonData (pokemonName) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    const pokemonData = await response.json();
+    return pokemonData;
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const pikachuData = await fetchPokemonData('charizard');
+    const pokemonInfoElement = document.getElementById('pokemon-info');
+
+    // Displaying pikachu data
+
+    pokemonInfoElement.innerHTML = `
+    <h2>${pikachuData.name}</h2>
+    <img src = "${pikachuData.sprites.front_default}" alt = "${pikachuData.name}">
+    <h3>Abilities:</h3>
+    <ul>
+        ${pikachuData.abilities.map(ability => `<li>${ability.ability.name}<li>`).join('')}
+    </ul>
+    <h3>Base Experience:</h3>
+    <p>${pikachuData.base_experience}</p>
+    `;
+})
